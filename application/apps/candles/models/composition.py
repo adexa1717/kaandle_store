b2b7163a18140870ac156b83605d839bd4da3a96
AdBerikkul <adexa1717@mail.ru>
2022-05-02 23:10:58 +0600

    temporary commit

--- application/apps/candles/models/composition.py
+++ application/apps/candles/models/composition.py
@@ -1,5 +1,10 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
-class CandleComposition(models.Model):
-    pass
+class Composition(models.Model):
+    name = models.CharField(max_length=50, verbose_name=_("name"))
+
+    class Meta:
+        verbose_name = _('composition')
+        verbose_name_plural = _('compositions')
--- application/apps/candles/models/form.py
+++ application/apps/candles/models/form.py
@@ -1,5 +1,11 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
-class CandleForm(models.Model):
-    pass
+class FormImages(models.Model):
+    path = models.ImageField()
+
+
+class Form(models.Model):
+    name = models.CharField(max_length=50, verbose_name=_("name"))
+    images = models.ForeignKey(to=FormImages, on_delete=models.CASCADE, related_name="forms")
